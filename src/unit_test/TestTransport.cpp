424337778d03be8f38ae176c4a1e1b7d4c592d97
Joegen Baclor <joegen@ossapp.com>
2015-09-27 12:06:39 +0800

    Updated unit-test

--- src/unit_test/TestTransport.cpp
+++ src/unit_test/TestTransport.cpp
@@ -32,67 +32,70 @@ std::string password_callback(
 
 TEST(TransportTest, test_tls_transport)
 {
-  boost::asio::ssl::context& tlsServerContext = tlsServer.tlsServerContext();
-  boost::asio::ssl::context& tlsClientContext = tlsServer.tlsClientContext();
+  if (boost::filesystem::exists(SERVER_KEY_FILE))
+  {
+    boost::asio::ssl::context& tlsServerContext = tlsServer.tlsServerContext();
+    boost::asio::ssl::context& tlsClientContext = tlsServer.tlsClientContext();
 
 
-  tlsClientContext.set_verify_mode( boost::asio::ssl::context::verify_peer);
-  tlsServerContext.set_verify_mode(boost::asio::ssl::context::verify_peer | boost::asio::ssl::context::verify_fail_if_no_peer_cert);
-  
- 
-  ASSERT_TRUE(boost::filesystem::exists(SERVER_KEY_FILE));
-  ASSERT_TRUE(boost::filesystem::exists(SERVER_CERT_FILE));
-  ASSERT_TRUE(boost::filesystem::exists(TLS_CA_FILE));
-  
- 
-  tlsServerContext.set_password_callback(password_callback);
-  tlsServerContext.use_certificate_file(SERVER_CERT_FILE, boost::asio::ssl::context::pem);
-  tlsServerContext.use_private_key_file(SERVER_KEY_FILE, boost::asio::ssl::context::pem);
-  tlsServerContext.load_verify_file(TLS_CA_FILE);
-  
-  tlsClientContext.set_password_callback(password_callback);
-  tlsClientContext.use_certificate_file(SERVER_CERT_FILE, boost::asio::ssl::context::pem);
-  tlsClientContext.use_private_key_file(SERVER_KEY_FILE, boost::asio::ssl::context::pem);
-  tlsClientContext.load_verify_file(TLS_CA_FILE);
+    tlsClientContext.set_verify_mode( boost::asio::ssl::context::verify_peer);
+    tlsServerContext.set_verify_mode(boost::asio::ssl::context::verify_peer | boost::asio::ssl::context::verify_fail_if_no_peer_cert);
 
-  
-  OSS::Net::IPAddress localAddress("127.0.0.1");
-  OSS::Net::IPAddress remoteAddress("127.0.0.1");
-  remoteAddress.setPort(35061);
-  
 
-  SIPListener::SubNets subnets;
-  subnets.push_back("0.0.0.0/0");
-  tlsServer.addTLSTransport("127.0.0.1", "35061", "127.0.0.1", subnets);
-  
-  tlsServer.run();
+    ASSERT_TRUE(boost::filesystem::exists(SERVER_KEY_FILE));
+    ASSERT_TRUE(boost::filesystem::exists(SERVER_CERT_FILE));
+    ASSERT_TRUE(boost::filesystem::exists(TLS_CA_FILE));
 
-  OSS::thread_sleep(100);
-  
-  SIPTransportSession::Ptr pClient = tlsServer.createClientTlsTransport(localAddress, remoteAddress);
-  
-  ASSERT_TRUE(pClient);
-  
-  OSS::thread_sleep(100);
-  
-  std::ostringstream msg;
-  msg << "OPTIONS sips:900@127.0.0.1:35061 SIP/2.0" << CRLF;
-  msg << "To: <sips:900@127.0.0.1>" << CRLF;
-  msg << "From: 9011 <sips:9011@127.0.0.1>;tag=6657e067" << CRLF;
-  msg << "Via: SIP/2.0/TLS 127.0.0.1:5061;branch=z9hG4bK-d87543-419889160-1--d87543-;rport" << CRLF;
-  msg << "Call-ID: 885e5e180c04c509" << CRLF;
-  msg << "CSeq: 1 OPTIONS" << CRLF;
-  msg << "Contact: <sips:9011@127.0.0.1:5061>" << CRLF << CRLF;
-  
-  SIPMessage::Ptr pMsg(new SIPMessage(msg.str()));
-  
-  pClient->writeKeepAlive();
-  pClient->writeMessage(pMsg);
 
-  
-  OSS::thread_sleep(100);
-  tlsServer.stop();
-  
-  ASSERT_TRUE(tlsDispatched);
+    tlsServerContext.set_password_callback(password_callback);
+    tlsServerContext.use_certificate_file(SERVER_CERT_FILE, boost::asio::ssl::context::pem);
+    tlsServerContext.use_private_key_file(SERVER_KEY_FILE, boost::asio::ssl::context::pem);
+    tlsServerContext.load_verify_file(TLS_CA_FILE);
+
+    tlsClientContext.set_password_callback(password_callback);
+    tlsClientContext.use_certificate_file(SERVER_CERT_FILE, boost::asio::ssl::context::pem);
+    tlsClientContext.use_private_key_file(SERVER_KEY_FILE, boost::asio::ssl::context::pem);
+    tlsClientContext.load_verify_file(TLS_CA_FILE);
+
+
+    OSS::Net::IPAddress localAddress("127.0.0.1");
+    OSS::Net::IPAddress remoteAddress("127.0.0.1");
+    remoteAddress.setPort(35061);
+
+
+    SIPListener::SubNets subnets;
+    subnets.push_back("0.0.0.0/0");
+    tlsServer.addTLSTransport("127.0.0.1", "35061", "127.0.0.1", subnets);
+
+    tlsServer.run();
+
+    OSS::thread_sleep(100);
+
+    SIPTransportSession::Ptr pClient = tlsServer.createClientTlsTransport(localAddress, remoteAddress);
+
+    ASSERT_TRUE(pClient);
+
+    OSS::thread_sleep(100);
+
+    std::ostringstream msg;
+    msg << "OPTIONS sips:900@127.0.0.1:35061 SIP/2.0" << CRLF;
+    msg << "To: <sips:900@127.0.0.1>" << CRLF;
+    msg << "From: 9011 <sips:9011@127.0.0.1>;tag=6657e067" << CRLF;
+    msg << "Via: SIP/2.0/TLS 127.0.0.1:5061;branch=z9hG4bK-d87543-419889160-1--d87543-;rport" << CRLF;
+    msg << "Call-ID: 885e5e180c04c509" << CRLF;
+    msg << "CSeq: 1 OPTIONS" << CRLF;
+    msg << "Contact: <sips:9011@127.0.0.1:5061>" << CRLF << CRLF;
+
+    SIPMessage::Ptr pMsg(new SIPMessage(msg.str()));
+
+    pClient->writeKeepAlive();
+    pClient->writeMessage(pMsg);
+
+
+    OSS::thread_sleep(100);
+    tlsServer.stop();
+
+    ASSERT_TRUE(tlsDispatched);
+  }
 }
 
